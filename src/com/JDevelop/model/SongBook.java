e81b22cfc000b2af3411b3b7f229f5eae99c6a6a
andyjery <jerry.chen1976@gmail.com>
2017-03-29 23:24:43 +0800

    Lesson on 3/28

--- src/com/JDevelop/model/SongBook.java
+++ src/com/JDevelop/model/SongBook.java
@@ -6,7 +6,10 @@
 package com.JDevelop.model;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  *
@@ -25,5 +28,31 @@ public class SongBook {
     
     public int getSongCount() {
         return mSongs.size();
+    }
+    
+    public Map<String, List<Song>> byArtist()
+    {
+        Map<String, List<Song>> byArtist = new HashMap<>();
+        for(Song song:mSongs)
+        {
+            List<Song> artistSongs = byArtist.get(song.getmArtist());
+            if (artistSongs == null)
+            {
+                artistSongs = new ArrayList<>();
+                byArtist.put(song.getmArtist(), artistSongs);
+            }
+            artistSongs.add(song);
+        }
+        return byArtist;
+    }
+    
+    public Set<String> getArtists()
+    {
+        return byArtist().keySet();
+    }
+    
+    public List<Song> getSongsForArtist(String artistName)
+    {
+        return byArtist().get(artistName);
     }
 }
