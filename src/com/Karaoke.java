bd25a1fd58fd3fb7838fc50238f29022cc4dc959
andyjery <jerry.chen1976@gmail.com>
2017-03-31 23:09:08 +0800

    Lesson on 3/30

--- src/com/Karaoke.java
+++ src/com/Karaoke.java
@@ -20,9 +20,11 @@ public class Karaoke {
     public static void main(String[] args) {
         // TODO code application logic here
         SongBook songBook = new SongBook();
+        songBook.importFrom("songs.txt");
         KaraokeMachine machine = new KaraokeMachine(songBook);
         machine.run();
-        
+        System.out.println("Saving book...........");
+        songBook.exportTo("songs.txt");
     }
     
 }
